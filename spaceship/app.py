63e001296c373c69514de3c8de809ad9103a63c5
andriy8075 <andriy8075@gmail.com>
2025-04-20 10:40:19 +0300

    print name

--- spaceship/app.py
+++ spaceship/app.py
@@ -7,6 +7,9 @@ from spaceship.routers import api, health
 
 
 def make_app(settings: Settings) -> FastAPI:
+
+    print("Ашомка Андрій")
+
     app = FastAPI(
         debug=settings.debug,
         title=settings.app_title,
